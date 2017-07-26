5f9bff210b10a2dd7c29cf5954e32679ae21998e
MARKAN\mark0 <kuchmynda95@gmail.com>
2017-07-26 20:46:47 +0300

    Добавьте файлы проекта.

---
+++ PrivateGallery/Activities/LoginScreen.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace PrivateGallery.Activities
+{
+    [Activity(Label = "Anonymous Gallery", MainLauncher = true, Theme = "@style/NoActionBar")]
+    public class LoginScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.PhotoEditScreen/*Resource.Layout.LoginScreen*/);
+            // Create your application here
+        }
+    }
+}
