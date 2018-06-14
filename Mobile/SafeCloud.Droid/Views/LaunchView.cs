9189f7ba1bf81f8892b3a81a56314d02e5c66680
Markiian Kuchmynda <mkuchmynda@vector-software.com>
2018-06-14 11:27:02 +0300

    LaunchScreen #1

---
+++ Mobile/SafeCloud.Droid/Views/LaunchView.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace SafeCloud.Droid.Views
+{
+    [Activity(Label = "SafeCloud", MainLauncher = true, NoHistory = true)]
+    public class LaunchView : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
